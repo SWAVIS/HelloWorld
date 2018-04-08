6fb9dcc3b781d4121e59117207bd57d7914a8877
VISHAL <vishalyadav2088@gmail.com>
2018-04-09 00:44:12 +0530

    initial commit

--- app/src/main/java/in/vishal/helloworld/MainActivity.kt
+++ app/src/main/java/in/vishal/helloworld/MainActivity.kt
@@ -9,6 +9,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
         Toast.makeText(this, getString(R.string.hello_world), Toast.LENGTH_LONG).show()
     }
 }
